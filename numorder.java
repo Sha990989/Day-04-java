14cdab70f58ab11979fec8cda56ff0e5cac652c6
Sha990989 <anavaneethasankar@gmail.com>
2024-12-05 21:53:25 +0530

    Add files via upload

---
+++ numorder.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class numorder
+{
+    public static void main(String args[])
+    {
+        int i=10;
+        int n=11;
+        while(i>0)
+        {
+            System.out.println(n-i);
+            i-=1;
+        }
+
+    }
+}
