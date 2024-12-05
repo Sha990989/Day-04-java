b5a60977cc42da6a65a3cd2407c00d89f6aea2f2
Sha990989 <anavaneethasankar@gmail.com>
2024-12-05 12:37:25 +0530

    Add files via upload

---
+++ ternary.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class ternary {
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        int a=s.nextInt();
+        int b=s.nextInt();
+        System.out.println((a>b)?"A is bigger":"B is bigger");
+    }
+}
