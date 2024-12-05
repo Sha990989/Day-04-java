d5d9fa4e87cb563f67f1cfe131ff3a9cd340d56a
Sha990989 <anavaneethasankar@gmail.com>
2024-12-05 12:45:13 +0530

    Add files via upload

---
+++ loopeven.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class loopeven {
+    public static void main(String args[])
+    {
+        int i;
+        for(i=0;i<=10;i+=2)
+        {
+            System.out.println(i);
+        }
+    }
+    
+}
