60b5b5189e30fe2782b18dbacccffacc18807f1f
Sha990989 <anavaneethasankar@gmail.com>
2024-12-05 21:50:13 +0530

    Add files via upload

---
+++ increanddecre.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class increanddecre
+{
+public static void main(String args[])
+{
+//post increment
+int a=5;
+System.out.println(a++);
+//pre increment
+int b=4;
+System.out.println(++b);
+//post decrement
+int c=6;
+System.out.println(c--);
+//pre decrement
+int d=6;
+System.out.println(--d);
+}
+}
