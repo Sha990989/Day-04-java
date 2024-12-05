898a57a35b1bc91c45acf7a9e4c47b32afef8eea
Sha990989 <anavaneethasankar@gmail.com>
2024-12-05 16:05:40 +0530

    Add files via upload

---
+++ checkingdivisor.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class checkingdivisor {
+    public static void main(String args[])
+{
+int n=16;
+for(int i=1;i*i<=n;i++)
+{
+    System.out.println(i);
+}
+}
+}
+    
+
